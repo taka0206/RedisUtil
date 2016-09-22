88ed74e1870e0234623777a9818e4545e67d9fed
å <taka0206@me.com>
2016-09-22 11:11:57 +0900

    [初回コミット] コミット

---
+++ src/main/java/jp/co/softbank/RedisUtil/RedisUtilMain.java
@@ -0,0 +1,33 @@
+package jp.co.softbank.RedisUtil;
+
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class RedisUtilMain implements ActionListener {
+
+	private JFrame mainFrame;
+	private Container contentPane;
+	private JPanel topPane;
+	private JPanel topLabel;
+	private JTextField textField;
+	private JPanel westPane;
+	private JLabel westLabel;
+	private JComboBox<Object> combo;
+	
+	
+	
+	
+	
+	public void actionPerformed(ActionEvent e) {
+		
+
+	}
+
+}
